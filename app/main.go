8c576c8810d0e700e8bbed88f3ce4c4d2503fcbe
Emeric de Bernis <emeric.debernis@qonto.com>
2022-05-11 09:01:41 +0200

    stage 2

--- app/main.go
+++ app/main.go
@@ -8,17 +8,15 @@ import (
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
-	fmt.Println("Logs from your program will appear here!")
-
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 
 	cmd := exec.Command(command, args...)
-	output, err := cmd.Output()
-	if err != nil {
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
 		fmt.Printf("Err: %v", err)
 		os.Exit(1)
 	}
-
-	fmt.Println(string(output))
 }
