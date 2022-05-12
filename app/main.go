fb620069ebb942d8735e4d31ae8b4edc2c168851
Emeric de Bernis <emeric.debernis@qonto.com>
2022-05-12 10:57:37 +0200

    stage 6

--- app/main.go
+++ app/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"archive/tar"
+	"compress/gzip"
+	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"syscall"
 )
 
@@ -40,8 +46,136 @@ func copy(src, dst string) error {
 	return os.Chmod(dst, info.Mode())
 }
 
+type registryTokenSvcResponse struct {
+	Token string `json:"token,omitempty"`
+}
+
+func registryLogin(image string) (string, error) {
+	imageSplit := strings.Split(image, ":")
+	url := fmt.Sprintf("https://auth.docker.io/token?service=registry.docker.io&scope=repository:library/%s:pull", imageSplit[0])
+
+	resp, err := http.DefaultClient.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer func() { _ = resp.Body.Close() }()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("failed to get docker registry token. Status code: %d", resp.StatusCode)
+	}
+
+	var response registryTokenSvcResponse
+	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+		return "", err
+	}
+
+	return response.Token, nil
+}
+
+type manifestResponse struct {
+	Layers []layer `json:"layers,omitempty"`
+}
+
+type layer struct {
+	MediaType string `json:"mediaType,omitempty"`
+	Size      int64  `json:"size,omitempty"`
+	Digest    string `json:"digest,omitempty"`
+}
+
+func fetchManifest(token, image string) (manifestResponse, error) {
+	imageSplit := strings.Split(image, ":")
+	url := fmt.Sprintf("https://registry.hub.docker.com/v2/library/%s/manifests/%s", imageSplit[0], imageSplit[1])
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return manifestResponse{}, err
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
+	req.Header.Add("Accept", "application/vnd.docker.distribution.manifest.v2+json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return manifestResponse{}, err
+	}
+	defer func() { _ = resp.Body.Close() }()
+
+	if resp.StatusCode != http.StatusOK {
+		return manifestResponse{}, fmt.Errorf("failed to get image manifest. Status code: %d", resp.StatusCode)
+	}
+
+	var response manifestResponse
+	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+		return manifestResponse{}, err
+	}
+
+	return response, nil
+}
+
+func extractLayer(token, image, digest, rootDir string) error {
+	imageSplit := strings.Split(image, ":")
+	url := fmt.Sprintf("https://registry.hub.docker.com/v2/library/%s/blobs/%s", imageSplit[0], digest)
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = resp.Body.Close() }()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to get image manifest. Status code: %d", resp.StatusCode)
+	}
+
+	gzr, err := gzip.NewReader(resp.Body)
+	if err != nil {
+		return err
+	}
+	defer gzr.Close()
+
+	tr := tar.NewReader(gzr)
+	for {
+		header, err := tr.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		target := filepath.Join(rootDir, header.Name)
+
+		switch header.Typeflag {
+		case tar.TypeDir:
+			if _, err := os.Stat(target); err != nil {
+				if err := os.MkdirAll(target, 0755); err != nil {
+					return err
+				}
+			}
+		case tar.TypeReg:
+			f, err := os.OpenFile(target, os.O_CREATE|os.O_RDWR, os.FileMode(header.Mode))
+			if err != nil {
+				return err
+			}
+			if _, err := io.Copy(f, tr); err != nil {
+				return err
+			}
+			f.Close()
+		}
+	}
+
+	return nil
+}
+
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
+	image := os.Args[2]
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 
@@ -51,6 +185,22 @@ func main() {
 	}
 	defer os.RemoveAll(dir)
 	chrootRoot := filepath.Join("/tmp", dir)
+
+	token, err := registryLogin(image)
+	if err != nil {
+		panic(err)
+	}
+
+	manifest, err := fetchManifest(token, image)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, layer := range manifest.Layers {
+		if err := extractLayer(token, image, layer.Digest, chrootRoot); err != nil {
+			panic(err)
+		}
+	}
 
 	commandDir := filepath.Dir(command)
 	commandName := filepath.Base(command)
